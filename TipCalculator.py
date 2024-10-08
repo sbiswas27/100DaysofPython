94f02f98d2a966ac221c503c1b187c9e36cc7386
sbiswas27 <88348998+sbiswas27@users.noreply.github.com>
2024-10-08 11:02:31 -0700

    Update TipCalculator.py

--- TipCalculator.py
+++ TipCalculator.py
@@ -1,4 +1,6 @@
 # Task: We're going to build a tip calculator.If the bill was $150.00, split between 5 people, with 12% tip.Each person should pay:(150.00 / 5) * 1.12 = 33.6. After formatting the result to 2 decimal places = 33.60
+# Demo link - https://appbrewery.github.io/python-day2-demo/
+
 
 #Welcome Greetings
 print("Welcome to the tip calculator!")
